64eb938ad9468dd796fb4d6df80967138c45cb99
381706-2leonipau <leonipau@yandex.ru>
2019-10-17 01:11:28 +0300

    Second commit

--- modules/task_1/pauzin_l_min_elem_in_vector/main.cpp
+++ modules/task_1/pauzin_l_min_elem_in_vector/main.cpp
@@ -1,59 +1,55 @@
-
-#include <iostream>
+// Copyright 2019 Pauzin Leonid
 #include <gtest-mpi-listener.hpp>
-#include <vector>
 #include <gtest/gtest.h>
+#include <vector>
 #include "./pauzin_l_min_elem_in_vector.h"
 
 TEST(Parallel_Min_In_Vector_MPI, Test_On_Default_Vec) { 
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> vec;
-	const int count_size_vector = 100;
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<int> vec;
+  const int count_size_vector = 100;
+  if (rank == 0) {
+    vec = getVec(count_size_vector);
+  }
 
-	if (rank == 0) {
-		vec = getVec(count_size_vector);
-	}
-
-	int minElem = minElemInVec(vec, count_size_vector);
-	if (rank == 0) {
-		ASSERT_EQ(0, minElem);
-	}
+  int minElem = minElemInVec(vec, count_size_vector);
+  if (rank == 0) {
+    ASSERT_EQ(0, minElem);
+  }
 } 
 
 TEST(Parallel_Min_In_Vector_MPI, Test_Vector_With_Given_Values) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> vec = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-	int minElem = minElemInVec(vec, vec.size());
-	if (rank == 0) {
-
-		ASSERT_EQ(minElem, 1);
-	}
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<int> vec = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+  int minElem = minElemInVec(vec, vec.size());
+  if (rank == 0) {
+    ASSERT_EQ(minElem, 1);
+  }
 }
 
 
 TEST(Parallel_Min_In_Vector_MPI, Test_On_Vec_With_Const) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	const int count_size_vector = 10;
-	std::vector<int> vec(count_size_vector);
-	if (rank == 0) {
-		for (int i = 0; i < count_size_vector; i++)
-		{
-			vec[i] = 7;
-		}
-	}
-	int minElem = minElemInVec(vec, count_size_vector);
-	if (rank == 0) {
-		ASSERT_EQ(7, minElem);
-	}
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  const int count_size_vector = 10;
+  std::vector<int> vec(count_size_vector);
+  if (rank == 0) {
+    for (int i = 0; i < count_size_vector; i++) {
+      vec[i] = 7;
+    }
+  }
+  int minElem = minElemInVec(vec, count_size_vector);
+  if (rank == 0) {
+    ASSERT_EQ(7, minElem);
+  }
 }
 
 
 int main(int argc, char** argv) {
 	::testing::InitGoogleTest(&argc, argv);
-	MPI_Init(&argc, &argv);
+  MPI_Init(&argc, &argv);
 
 	::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
 	::testing::TestEventListeners& listeners =
--- modules/task_1/pauzin_l_min_elem_in_vector/pauzin_l_min_elem_in_vector.h
+++ modules/task_1/pauzin_l_min_elem_in_vector/pauzin_l_min_elem_in_vector.h
@@ -1,6 +1,6 @@
-//Copyright 2019 Pauzin Leonid
-#ifndef MODULES_TASK_1_PAUZIN_L_MIN_ELEM_IN_VECTOR_H_MIN_ELEM_IN_VECTOR_H_
-#define MODULES_TASK_1_PAUZIN_L_MIN_ELEM_IN_VECTOR_H_MIN_ELEM_IN_VECTOR_H_
+// Copyright 2019 Pauzin Leonid
+#ifndef MODULES_TASK_1_PAUZIN_L_MIN_ELEM_IN_VECTOR_PAUZIN_L_MIN_ELEM_IN_VECTOR_H_
+#define MODULES_TASK_1_PAUZIN_L_MIN_ELEM_IN_VECTOR_PAUZIN_L_MIN_ELEM_IN_VECTOR_H_
 
 #include <vector>
 #include <string>
@@ -12,4 +12,4 @@ int minElemInVec(const std::vector <int> vec, int n);
 int getSequentialMin(std::vector<int> vec);
 
 
-#endif //MODULES_TASK_1_PAUZIN_L_MIN_ELEM_IN_VECTOR_H_
+#endif  // MODULES_TASK_1_PAUZIN_L_MIN_ELEM_IN_VECTOR_PAUZIN_L_MIN_ELEM_IN_VECTOR_H_
